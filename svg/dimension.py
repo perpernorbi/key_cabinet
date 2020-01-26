8b96ce47ae6fa084b9bb9d80a3d402a038f3ec9b
Norbert Koszo <k.norbi@gmail.com>
2020-01-26 21:56:54 +0100

    Add dimension and sheet

---
+++ svg/dimension.py
@@ -0,0 +1,46 @@
+from svgwrite.text import Text
+from svgwrite.container import Group
+from svgwrite.shapes import Line
+
+import math
+
+def dimension(insert, size, font_size=0.8, offset=0, **kwargs):
+    offset = offset + font_size * 7
+    x, y = insert[0], insert[1] + offset
+    width, height = size
+    l = math.sqrt(width ** 2 + height ** 2)
+    angle = 90*height/abs(height) if width == 0 \
+        else math.degrees(math.atan(height/width))
+    if width < 0:
+        angle = 180 + angle
+    e = 1
+    endtick = lambda i: ((i[0]-e, i[1]+e), (i[0]+e, i[1]-e))
+    g = Group()
+    g.add(Line(insert, (x, y), **kwargs))
+    g.add(Line((x + l, insert[1]), (x + l,y), **kwargs))
+    g.add(Line(*endtick((x, y)), **kwargs))
+    g.add(Line(*endtick((x + l, y)), **kwargs))
+    g.add(Line((x, y), (x + l, y), **kwargs))
+
+    text_group = Group()
+    text = int(l) if l.is_integer() else f'{l:.1f}'
+    text_group.add(Text(text, (x + l / 2.0, y - e), text_anchor="middle", alignment_baseline="after-edge",
+                        style="font-style:normal;"
+                              "font-variant:normal;"
+                              "font-weight:normal;"
+                              "font-stretch:normal;"
+                              f"font-size:{font_size}mm;"
+                              "line-height:1.25;"
+                              "font-family:Bariol;"
+                              "-inkscape-font-specification:Bariol;"
+                              "letter-spacing:0px;"
+                              "word-spacing:0px;"
+                              "fill:#000000;"
+                              "fill-opacity:1;"
+                              "stroke:none;"
+                              "stroke-width:0.26012909"))
+    if 45 < angle <= 225:
+        text_group.rotate(180, (x+l/2, y))
+    g.add(text_group)
+    g.rotate(angle, center=insert)
+    return g
--- svg/sheet.py
+++ svg/sheet.py
@@ -1,5 +1,6 @@
 from svgwrite.container import Group
 from svgwrite.shapes import Rect
+from svgwrite.text import Text
 
 
 def sheet():
@@ -7,10 +8,17 @@ def sheet():
     g = Group()
     g.add(Rect((10, 10), (190, 277), **l))
 
-    g.add(Rect((10, 10), (60, 7.5), **l))
-    g.add(Rect((10, 10), (60, 15), **l))
-    g.add(Rect((180, 10), (20, 7.5), **l))
-    g.add(Rect((180, 10), (20, 15), **l))
-    g.add(Rect((10, 10), (190, 15), **l))
+    g.add(Rect((10, 297-15-10), (60, 7.5), **l))
+    g.add(Rect((10, 297-15-10), (60, 15), **l))
+    g.add(Rect((180, 297-15-10), (20, 7.5), **l))
+    g.add(Rect((180, 297-15-10), (20, 15), **l))
+    g.add(Rect((10, 297-15-10), (190, 15), **l))
+    style = "font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:5px;line-height:1.25;font-family:Bariol;-inkscape-font-specification:Bariol;letter-spacing:0px;word-spacing:0px;fill:#000000;fill-opacity:1;stroke:none;stroke-width:0.26012909"
+    g.add(Text(f"Koszó Norbert", (15, 297-15-10+15/4.0), text_anchor="start", alignment_baseline="middle", style=style))
+    g.add(Text(f"2020. január 06.", (15, 297-15-10+3*15/4.0), text_anchor="start", alignment_baseline="middle", style=style))
+    g.add(Text(f"Valami megnevezés", (15 + 60, 297-15-10+2*15/4.0), text_anchor="start", alignment_baseline="middle", style=style))
+    g.add(Text(f"1:5", (182, 297-15-10+1*15/4.0), text_anchor="start", alignment_baseline="middle", style=style))
+    g.add(Text(f"2019/62", (182, 297-15-10+3*15/4.0), text_anchor="start", alignment_baseline="middle", style=style))
+
     return g
 
